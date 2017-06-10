d95b5cc2f0f9fd925c46de944d4d47bca05e2834
rajdeeprath <rajdeeprath@gmail.com>
2017-06-11 05:27:49 +0530

    Red5Js plugin base
    + Added Red5Js plugin base

---
+++ red5jsplugin/src/main/java/com/flashvisions/server/red5/plugin/api/Red5JsPlugin.java
@@ -0,0 +1,32 @@
+package com.flashvisions.server.red5.plugin.api;
+
+import org.red5.server.plugin.Red5Plugin;
+
+public class Red5JsPlugin extends Red5Plugin {
+	
+	
+	@Override
+	public void doStart() throws Exception {
+		// TODO Auto-generated method stub
+		super.doStart();
+	}
+
+	@Override
+	public void doStop() throws Exception {
+		// TODO Auto-generated method stub
+		super.doStop();
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		super.init();
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return super.getName();
+	}
+
+}
